f52a7b90f9487f1639b46ba2735584b272e1a008
Christian Rosado <christian.rosado20@upr.edu>
2020-05-06 22:27:37 -0400

    Creating website with react.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Boricua Code</h1>
+      <p>Under construction...</p>
+    </div>
+  );
+}
+
+export default App;
